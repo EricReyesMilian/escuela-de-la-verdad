791dedc9577c0a10ae24ff7513eab09b23eafd22
Eric Reyes <ericruper73@gmail.com>
2025-08-02 16:27:00 -0400

    font

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 import Header from './components/Header'
 import DownloadButtons from './components/DownloadButtons'
 import './index.css'
-
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 function App() {
   return (
     <div className="app">
